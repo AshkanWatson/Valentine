d6382fa38f808aae637f223f69a8192c67d5a213
Ashkan Watson <AshkanWatson@gmail.com>
2026-02-11 15:23:03 +0330

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Valentine Letter",
+  description: "A cute Valentine's Day ask",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/icon.jpg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Aldrich&family=Pixelify+Sans:wght@400..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
